12b227c9dd05fed74f280f5e82abef7b05b776aa
sea835 <148954003+sea835@users.noreply.github.com>
2026-09-25 15:11:51 +0700

    refactor: split auth module into separate authentication and authorization modules with CASL policies

--- src/modules/auth/presentation/decorators/roles.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import type { RoleType } from '@modules/user/domain/user-role';
-
-export const ROLES_KEY = 'roles';
-export const Roles = (...roles: RoleType[]) => SetMetadata(ROLES_KEY, roles);
